42258934499410b9cfecae909a3b99a49c2b6bf0
oscar <oscar.w.ergun@gmail.com>
2025-06-08 09:27:57 +0200

    init

---
+++ Presentation/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+var app = builder.Build();
+app.MapOpenApi();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
